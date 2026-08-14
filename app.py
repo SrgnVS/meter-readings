31d2609e2db765b2d756f69e0ad3958bcbb7596a
SrgnVS <Gva_uni21@mail.ru>
2026-08-14 16:41:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask, request, jsonify, send_file, send_from_directory
+from flask_cors import CORS
+import os
+import sqlite3
+from datetime import datetime, timedelta, timezone
+import csv
+from io import StringIO
+import requests
+import html
+
 app = Flask(__name__)
+CORS(app)  # разрешаем запросы с любых доменов
 
 @app.route('/')
 def index():
@@ -11,7 +21,9 @@ def ping():
 
 @app.route('/recognize', methods=['GET', 'POST'])
 def recognize():
-    return "OK", 200
+    if request.method == 'GET':
+        return "OCR endpoint is working (EasyOCR disabled).", 200
+    return jsonify({"digits": "12345"}), 200
 
 if __name__ == '__main__':
     app.run()
